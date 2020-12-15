e4d83ab09747330c8366a68e1a258c54f864dc52
daisuke524 <daisuke.u524@gmail.com>
2020-12-16 07:10:11 +0900

    削除

--- velocity.py
+++
@@ -1,9 +0,0 @@
-def velocity(distance, second):
-    li = [distance, second]
-    li_in_str = list(map(str, li))
-    
-    v = distance / second
-    return li_in_str
-
-var_velocity = velocity(5.0, 3.32)
-print(var_velocity)
